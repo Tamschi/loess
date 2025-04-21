3775eec385edb3a7c865b38916a2216b5119413f
Tamme Schichler <tamme@schichler.dev>
2025-04-21 19:37:53 +0200

    Updated how Loess is used in Asteracea

--- proc-macro-definitions/src/components/component.rs
+++ proc-macro-definitions/src/components/component.rs
@@ -1,18 +1,17 @@
 use loess::{
-	grammar,
+	grammar, quote_into_with_exact_span,
 	rust_grammar::{
 		Async, Const, CurlyBraces, Identifier, Parentheses, RArrow, SquareBrackets, Visibility,
 	},
 	IntoTokens, SimpleSpanned,
 };
 use proc_macro2::{TokenStream, TokenTree};
-use quote::quote_spanned;
 use statements::Statement;
 
 pub mod statements;
 
 grammar! {
-	pub struct Component: PopFrom, IntoTokens {
+	pub struct Component: PopFrom {
 		pub visibility: Option<Visibility>,
 		pub r#const: Option<Const>,
 		pub r#async: Option<Async>,
@@ -25,8 +24,8 @@ grammar! {
 	}
 }
 
-impl Component {
-	pub fn transform(self, root: &TokenStream, tokens: &mut impl Extend<TokenTree>) {
+impl IntoTokens for Component {
+	fn into_tokens(self, root: &TokenStream, tokens: &mut impl Extend<TokenTree>) {
 		let Self {
 			visibility,
 			r#const,
@@ -38,15 +37,9 @@ impl Component {
 			substrate,
 			body,
 		} = self;
-		let visibility = visibility.collect_tokens::<TokenStream>(root);
 
-		let span = name.span();
-		let name: TokenStream = name.collect_tokens(&TokenStream::new());
-
-		// The verbatim span appears to be necessary here to show warnings about unused structs.
-		quote_spanned! {span=>
-			#visibility struct #name {}
-		}
-		.into_tokens(root, tokens)
+		quote_into_with_exact_span!(name.span(), root, tokens, [
+			{#paste visibility } struct {#paste name } {}
+		]);
 	}
 }
